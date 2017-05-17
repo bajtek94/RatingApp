4cbf99e4bc3b06001b0bd33e8b8c1fe7ef57c7dd
bajtek94 <damimiko94@gmail.com>
2017-05-17 09:40:17 +0200

    post class

---
+++ src/main/java/com/example/ratingapp/model/Post.java
@@ -0,0 +1,68 @@
+package com.example.ratingapp.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Bajtek on 17.05.2017.
+ */
+@Entity
+@Table(name = "post")
+public class Post {
+    private Long id;
+    private String title;
+    private String description;
+    private Long likes;
+    private Long dislikes;
+    private Long userId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Long likes) {
+        this.likes = likes;
+    }
+
+    public Long getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(Long dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    @Column(name = "user_id")
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
